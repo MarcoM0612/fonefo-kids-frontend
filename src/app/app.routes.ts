6be89aa52ecb610d28244aab18e7014ceedb9f9a
Marco Muñoz <marcoa.2132@gmail.com>
2025-06-11 18:52:12 -0500

    [+] Se crea nuevo proyecto de Fonefo-kids-frontend con angular [+] se crean componentes [+] se crean Layout (header)+(footer) and la carpeta Pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/public/home/home';
+import { Login } from './pages/public/login/login';
+import { Register } from './pages/public/register/register';
+import { User } from './pages/private/user/user';
+import { Tips } from './pages/private/tips/tips';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: Home },
+    { path: 'login', component: Login },
+    { path: 'register', component: Register},
+    { path: 'user', component: User},
+    { path: 'tips', component: Tips},
+    { path: '**',redirectTo: '', pathMatch: 'full'},
+    { path: '', redirectTo: 'home', pathMatch: 'full'}
+    
+];
