6902a4e433e47f64f8cc214a9378664bb0ff4225
Marco Muñoz <marcoa.2132@gmail.com>
2025-06-24 18:15:32 -0500

    [+] estructura del formulario de tips con su respectiva validación

--- src/app/pages/private/tips/new-form/new-form.ts
+++ src/app/pages/private/tips/new-form/new-form.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Users } from '../../../../services/users';
+
 
 @Component({
   selector: 'app-new-form',
@@ -8,18 +10,51 @@ import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
   styleUrl: './new-form.css'
 })
 export class tipNewForm {
-  formData!: FormGroup
+  formData!: FormGroup;
+  users: any = [];
 
-  constructor() {
+  constructor( private userServices: Users ) {
     this.formData = new FormGroup({
-      titulo: new FormControl(),
-      contenido: new FormControl(),
-      tipo: new FormControl(),
-      edad: new FormControl(),
-      autor: new FormControl()
+      titulo: new FormControl('', [Validators.required, Validators.minLength(5), Validators.maxLength(50) ]),
+      contenido: new FormControl('', [ Validators.required, Validators.minLength(5) ]),
+      tipo: new FormControl('', [Validators.required]),
+      edadRecomendada: new FormControl('', [Validators.required]),
+      autor: new FormControl() //TODO: traer los datos antes de establecer las reglas. 
     });
   }
   onSubmit () {
-    console.log(this.formData.value)
+    console.log(
+      this.formData.valid,
+      this.formData.invalid,
+      this.formData.pristine,
+      this.formData.dirty,
+      this.formData.touched
+    );
+
+    if ( this.formData.valid ) {
+      console.log(this.formData.value);
+    }
+    this.formData.reset();
   }
+  
+  
+  ngOnInit() {
+    this.userServices.getUsers().subscribe({
+      next: ( data ) => {
+        console.log (data);
+        this.users = data;
+      },
+      error: ( error ) => {
+        console.error( error )
+      },
+      complete: () => {
+        console.log( 'complete')
+      }
+    })
+  }
+  
+  ngOnDestroy() {
+    console.log( 'ngOnDestroy' );
+  }
+
 }
