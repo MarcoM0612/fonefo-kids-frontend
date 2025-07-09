5475de86e730d85e35b507d414d8ddf7d09b30e6
Marco Muñoz <marcoa.2132@gmail.com>
2025-07-08 22:17:15 -0500

    [+] se crea el formulario de crear producto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,6 +10,7 @@ import { authGuard } from './guards/auth-guard';
 import { Catalogo } from './pages/public/catalogo/catalogo';
 import { Nosotros } from './pages/public/nosotros/nosotros';
 import { Contacto } from './pages/public/contacto/contacto';
+import { ProductoNuevo } from './pages/private/productos/producto-nuevo/producto-nuevo';
 
 
 export const routes: Routes = [
@@ -22,6 +23,7 @@ export const routes: Routes = [
     { path: 'dashboard', component: Dashboard, canActivate: [ authGuard ] },
     { path: 'admin/user', component: User, canActivate: [ authGuard ] },
     { path: 'admin/tips', component: Tips, canActivate: [ authGuard ]},
+    { path: 'admin/producto/new', component: ProductoNuevo , canActivate:[ authGuard ]}, 
     { path: 'admin/tips/new', component: tipNewForm, canActivate: [ authGuard ] },
     { path: '**',redirectTo: 'home', pathMatch: 'full'},
     { path: '', redirectTo: 'home', pathMatch: 'full'}
